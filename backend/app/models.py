5ad76732cb276d62b859d19210abddaa8c414590
archi-007 <archiribhu@gmail.com>
2020-05-13 21:19:07 +0530

    added models

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,4 +4,11 @@ from django.db import models
 
 
 class Modelone(models.Model):
-    name = models.CharField(length = 100)
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    message = models.TextField()
+    created  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
